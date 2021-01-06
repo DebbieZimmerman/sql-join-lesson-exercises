9a77937d0c3945aa2b7279365324ff58118c8b4e
DebbieZimmerman <debbieseuss@gmail.com>
2021-01-06 23:25:28 +0200

    finished exercises

--- solutions/ex_6.sql
+++ solutions/ex_6.sql
@@ -1,1 +1,8 @@
 USE sql_intro;
+
+SELECT name, COUNT (*)
+FROM patient, ethnicity
+WHERE patient.disease = "lettuce disease"
+AND patient.ethnicity = ethnicity.id
+GROUP BY patient.ethnicity
+ORDER BY patient.ethnicity;
